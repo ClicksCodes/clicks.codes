a64c0b29fc1d2e548ce8885e440ca1a44791030f
Samuel Shuert <samuel@clicksminuteper.net>
2021-01-26 17:29:17 -0500

    fix?

--- pages/api/validate.js
+++ pages/api/validate.js
@@ -2,7 +2,7 @@ import JWT from 'jsonwebtoken';
 import fs from 'fs';
 
 export default async (req, res) => {
-    let publicKey = fs.readFileSync('./keys/rsm.pub');
+    let publicKey = fs.readFileSync('./keys/rsm.pub').toString();
     var decoded = JWT.verify(req.body.data.jwt, publicKey);
     return res.status(200).send(decoded)
 }
