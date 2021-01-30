e5bdd0ee5b5ca6504b59d28df99d736581a19dea
root <root@clicksminuteper.net>
2021-01-30 16:38:28 +0000

    Just in case someone had the brilliant idea of modifying stuff on the server

--- pages/api/validate.js
+++ pages/api/validate.js
@@ -2,7 +2,7 @@ import JWT from 'jsonwebtoken';
 import fs from 'fs';
 
 export default async (req, res) => {
-    let publicKey = fs.readFileSync('./keys/rsm.pub').toString();
-    var decoded = JWT.verify(req.body.data.jwt, publicKey);
+    let publicKey = fs.readFileSync('./keys/rsm.pub');
+    var decoded = JWT.verify(req.body.data.jwt, publicKey, {algorithms: ['SH256']});
     return res.status(200).send(decoded)
 }
