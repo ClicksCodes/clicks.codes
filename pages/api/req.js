e361345dfbf09c9878f6959d527c5f39374e490a
pineapplefan1234yt <pineapplefanyt@gmail.com>
2021-02-06 17:57:10 +0000

    hide secret?

--- pages/api/req.js
+++ pages/api/req.js
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 
-export default (req, res) => {
+export default async (req, res) => {
     let resp = await Axios.post(
         `https://192.168.102.5:10000/role/gid/${cls.props.gID}/rid/${cls.props.rID}/user/${cls.props.uID}/secret/${secret}}`
     )
