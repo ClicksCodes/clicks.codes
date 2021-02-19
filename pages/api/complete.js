91e800ce71977eb0570b1ee493a2f9f27ca3219f
pineapplefan1234yt <pineapplefanyt@gmail.com>
2021-02-19 19:23:15 +0000

    data -> body

--- pages/api/complete.js
+++ pages/api/complete.js
@@ -3,7 +3,7 @@ import Axios from 'axios';
 export default async (req, res) => {
     let secret = "slwu0rZV5W6WdmGtgI16du8Ar2tQGMr3Q9dE6u3poKiVODNV9SweaA3buawgkTmTuITXDWOUpBcTFA0qWrUvoshi1JB180WOFwA7"
     let resp = await Axios.post(
-        `https://rsm.bots.clicksminuteper.net/role/gid/${req.data.gid}/rid/${req.data.rid}/user/${req.data.uid}/secret/${secret}}`
+        `https://rsm.bots.clicksminuteper.net/role/gid/${req.body.gid}/rid/${req.body.rid}/user/${req.body.uid}/secret/${secret}}`
     )
     return resp.status;
 }
