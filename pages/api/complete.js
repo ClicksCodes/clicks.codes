0b853dda84317a28447b8322a58acc8dd6b0d5bd
pineapplefan1234yt <pineapplefanyt@gmail.com>
2021-02-19 19:13:01 +0000

    cls -> req

--- pages/api/complete.js
+++ pages/api/complete.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 
 export default async (req, res) => {
     let resp = await Axios.post(
-        `https://rsm.bots.clicksminuteper.net/role/gid/${cls.props.gID}/rid/${cls.props.rID}/user/${cls.props.uID}/secret/${secret}}`
+        `https://rsm.bots.clicksminuteper.net/role/gid/${req.props.gID}/rid/${req.props.rID}/user/${req.props.uID}/secret/${secret}}`
     )
     return resolve(resp.status);
 }
