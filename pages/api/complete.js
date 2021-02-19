c9483b801f37a4a63e0e252649e23296bf1a9eeb
pineapplefan1234yt <pineapplefanyt@gmail.com>
2021-02-19 19:29:19 +0000

    remove ddouble curly brace

--- pages/api/complete.js
+++ pages/api/complete.js
@@ -3,7 +3,7 @@ import Axios from 'axios';
 export default async (req, res) => {
     let secret = "slwu0rZV5W6WdmGtgI16du8Ar2tQGMr3Q9dE6u3poKiVODNV9SweaA3buawgkTmTuITXDWOUpBcTFA0qWrUvoshi1JB180WOFwA7"
     let resp = await Axios.post(
-        `http://rsm.bots.clicksminuteper.net/role/gid/${req.body.gid}/rid/${req.body.rid}/user/${req.body.uid}/secret/${secret}}`
+        `http://rsm.bots.clicksminuteper.net/role/gid/${req.body.gid}/rid/${req.body.rid}/user/${req.body.uid}/secret/${secret}`
     )
     return resp.status;
 }
