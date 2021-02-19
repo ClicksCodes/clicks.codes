d2da888a025e6c8d97361fc13eee860cfa5c2927
pineapplefan1234yt <pineapplefanyt@gmail.com>
2021-02-19 20:22:17 +0000

    http -> https

--- pages/api/complete.js
+++ pages/api/complete.js
@@ -5,7 +5,7 @@ export default async (req, res) => {
 
     let secret = "slwu0rZV5W6WdmGtgI16du8Ar2tQGMr3Q9dE6u3poKiVODNV9SweaA3buawgkTmTuITXDWOUpBcTFA0qWrUvoshi1JB180WOFwA7"
     let resp = await Axios.get(
-        `http://192.168.102.5:10000/role/gid/${req.body.gid}/rid/${req.body.rid}/user/${req.body.uid}/secret/${secret}`
+        `https://192.168.102.5:10000/role/gid/${req.body.gid}/rid/${req.body.rid}/user/${req.body.uid}/secret/${secret}`
     )
     return resp.status;
 }
