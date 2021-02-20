a0ac0ceb3220428459e08a00480b91ff211792b0
pineapplefan1234yt <pineapplefanyt@gmail.com>
2021-02-20 15:51:52 +0000

    fixed codes, removed a lotta logs

--- pages/api/complete.js
+++ pages/api/complete.js
@@ -3,24 +3,15 @@ import Axios from 'axios';
 export default async (req, res) => {
     let code = await Axios.post('http://localhost:3000/api/validate', {code:req.body.code});
     if (code.status != 200) {
-        console.log(code.status)
         return res.send(404);
     }
-    console.log(code.data)
-    console.log(code.data.user)
-    console.log(code.data.guild)
-    console.log(code.data.role)
-    console.log(req.body.uid)
-    console.log(req.body.gid)
-    console.log(req.body.rid)
-    if (code.data.user  != req.body.uid) return res.send(400)
-    if (code.data.guild != req.body.gid) return res.send(400)
-    if (code.data.role  != req.body.rid) return res.send(400)
+    if (code.data.user  != req.body.uid) return res.send(401)
+    if (code.data.guild != req.body.gid) return res.send(401)
+    if (code.data.role  != req.body.rid) return res.send(401)
 
     let secret = "slwu0rZV5W6WdmGtgI16du8Ar2tQGMr3Q9dE6u3poKiVODNV9SweaA3buawgkTmTuITXDWOUpBcTFA0qWrUvoshi1JB180WOFwA7"
     let resp = await Axios.get(
         `http://192.168.102.5:10000/role/gid/${req.body.gid}/rid/${req.body.rid}/user/${req.body.uid}/secret/${secret}/code/${req.body.code}`
     )
-    console.log("completed rsm request")
     return res.send(resp.status);
 }
