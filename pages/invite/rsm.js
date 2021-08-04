733240f697d20ed5e47811ea4bf040640ad50153
Samuel Shuert <samuel@clicksminuteper.net>
2021-08-04 16:25:19 -0400

    Update rsm.js

--- pages/invite/rsm.js
+++ pages/invite/rsm.js
@@ -1,1 +1,18 @@
+import { Component } from 'react'
 
+class Redirect extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+        <>
+          {window.location.replace('https://discord.com/api/oauth2/authorize?client_id=715989276382462053&permissions=121295465718&scope=bot%20applications.commands');}
+        </>
+        )
+    }
+
+}
+
+export default Redirect;
