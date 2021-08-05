1163a77962c3b066243a534a0c0fa79aa0953c48
Pineapplefan <pineapplefanyt@gmail.com>
2021-08-05 09:09:35 +0100

    Update rsm.js

--- pages/invite/rsm.js
+++ pages/invite/rsm.js
@@ -8,7 +8,7 @@ class Redirect extends Component {
     render() {
         return (
         <>
-            {/*<Redirect push to="https://discord.com/api/oauth2/authorize?client_id=715989276382462053&permissions=121295465718&scope=bot%20applications.commands" />*/}
+          {window.location.replace('https://discord.com/api/oauth2/authorize?client_id=715989276382462053&permissions=121295465718&scope=bot%20applications.commands');}
         </>
         )
     }
