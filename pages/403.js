7fb5d359ee2f844a2ea99cd860fc3b6ae2184b97
pineafan <pineapplefanyt@gmail.com>
2021-12-11 15:46:48 +0000

    fixes and easter eggs

---
+++ pages/403.js
@@ -0,0 +1,24 @@
+import Header from '../Components/Header'
+import {Component} from 'react'
+
+export default class Home extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+
+	render() {
+		return (
+			<>
+			<Header
+				name="403"
+				subtext="Forbidden - You do not have the correct permissions to view this page"
+				gradient={["F27878", "D96B6B"]}
+				wave="web/waves/header/rsm"
+				buttons={[{color: "F27878", buttonText: "ffffff", text: "Go Home", link: "/#"}]}
+        		hideArrow={true}
+			/>
+			</>
+		)
+	}
+}
