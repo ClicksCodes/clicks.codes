3075bf617c39cecc7a2fc6a3c7066c7112347d86
Pineapplefan <pineapplefanyt@gmail.com>
2021-12-27 17:29:51 +0000

    console.log

--- pages/api/rsmv/validate.js
+++ pages/api/rsmv/validate.js
@@ -2,6 +2,7 @@ import Axios from 'axios';
 
 const Validate = async (req, res) => {
         var out = await Axios.get(`http://192.168.102.7:10000/verify/${req.body.code}`)
+        console.log(out.status)
         if ( out.status !== 200 ) {
             return res.status(400)
         }
