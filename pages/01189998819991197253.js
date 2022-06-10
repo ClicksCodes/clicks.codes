0e357049d2f1c1585f5a22f36f0bcd08d1aaaedb
Samuel Shuert <samuel@clicksminuteper.net>
2022-06-10 16:12:51 -0400

    yes

--- pages/01189998819991197253.js
+++ pages/01189998819991197253.js
@@ -1,27 +1,6 @@
 import Header from '../Components/Header'
 
 export default function Error() {
-<<<<<<< HEAD
-    return (
-        <>
-            <Header
-                name="0118 999 881 999 119 7253"
-                subtext={<>
-                    Dear Sir/Madam,<br />
-                    Fire! Fire! Help me!<br />
-                    123 Cavendon Road.<br />
-                    Looking forward to hearing from you,<br />
-                    All the best, Maurice Moss
-                </>}
-                embedDescription="This is the internet"
-                gradient={["F27878", "D96B6B"]}
-                wave="web/waves/header/rsm"
-                buttons={[{color: "F27878", buttonText: "ffffff", text: "No that's too formal", link: "/#"}]}
-                fullscreen={true}
-            />
-        </>
-    )
-=======
 	return (
 		<>
 			<Header
@@ -41,5 +20,4 @@ export default function Error() {
 			/>
 		</>
 	)
->>>>>>> 11dc35cde9cce7a8dfe7f5000060b0ea04437844
 }
