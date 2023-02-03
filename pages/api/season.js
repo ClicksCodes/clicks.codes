37476d2c1e59cfae499af564a97fea70b3095761
PineappleFan <pineapplefanyt@gmail.com>
2023-02-03 21:36:40 +0000

    Added april fools to API

--- pages/api/season.js
+++ pages/api/season.js
@@ -5,14 +5,16 @@ function getSeason() {
         pride: [new Date(`${year}-6-1`), new Date(`${year}-6-31`)],
         halloween: [new Date(`${year}-10-25`), new Date(`${year}-11-1`)],
         trans: [new Date(`${year}-11-13`), new Date(`${year}-11-19`)],
-        christmas: [new Date(`${year}-12-1`), new Date(`${year}-12-26`)]
+        christmas: [new Date(`${year}-12-1`), new Date(`${year}-12-26`)],
+        aprilFools: [new Date(`${year}-04-01`), new Date(`${year}-04-01`)]
     }
     const filePaths = {
         normal: "normal",
         halloween: "seasonal/halloween",
         christmas: "seasonal/christmas",
         pride: "seasonal/pride",
-        trans: "seasonal/trans"
+        trans: "seasonal/trans",
+        aprilFools: "seasonal/aprilFools"
     }
 
     let current = new Date();
