85b4cc46a01a32fcb061b92615c5daea0561b8a0
PineappleFan <pineapplefanyt@gmail.com>
2023-03-01 08:20:00 +0000

    Update season.js

--- pages/api/season.js
+++ pages/api/season.js
@@ -18,8 +18,7 @@ function getSeason() {
         halloween: [new Date(`${year}-10-25`), new Date(`${year}-11-1`)],
         trans: [new Date(`${year}-11-13`), new Date(`${year}-11-19`)],
         christmas: [new Date(`${year}-12-1`), new Date(`${year}-12-26`)],
-        aprilFools: [new Date(`${year}-04-01`), new Date(`${year}-04-01`)],
-        fake: [new Date(`${year}-01-01`), new Date(`${year}-01-01`)]
+        aprilFools: [new Date(`${year}-04-01`), new Date(`${year}-04-01`)]
     }
     const filePaths = {
         normal: "normal",
